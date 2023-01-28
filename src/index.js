5a5b63875fbf897ec9c4b63a98f8d0fbeb27bd36
MaksymchukA <nastena_1991@ukr.net>
2023-01-28 18:27:59 +0200

    did a part of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import { getRefs } from './getRefs';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = getRefs();
+
+refs.searchCountryInput.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
+
+function searchCountry(event) {
+    let country = refs.searchCountryInput.value;
+
+    return fetchCountries(country).then((data) => {
+        choseCountry(data)
+    }).catch().finally()
+}
+
+function choseCountry(data) {
+    const markup = data.map(element => {
+        return `
+            <li>
+            <img src="${element.flags.svg}" alt="${element.name.official}" width="40" height="20">
+            <p>${element.name.official}</p>
+            </li>`;
+          }).join('');
+      
+          refs.countryList.innerHTML = markup;
+}
+
+function createCountryInfo(data) {
+    const markup = data.map(element => {
+    return `
+    <img src=${element.flags.svg} alt="${element.name.official}" width="40" height="20">
+    <h2>${element.name.official}</h2>
+    <p>Capital: ${element.capital}</p>
+    <p>Population: ${element.population}</p>    
+    <p>Languages: ${element.languages}</p>
+    `
+    }).join('');
+
+    refs.countryInfo.innerHTML = markup;
+}
+
+// function updateCountryInfo(markup) {
+//     refs.countryInfo.insertAdjacentElement('beforeend', createCountryInfo(markup))
+// }
